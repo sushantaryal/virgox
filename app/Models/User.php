0f169f8bc31183ff5b86e5a4acf7ba403822cae5
Sushant Aryal <sushant.aryal90@gmail.com>
2025-12-11 22:50:00 +0545

    Add assets relationship to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -46,5 +47,13 @@ class User extends Authenticatable
             'password' => 'hashed',
             'balance' => 'decimal:2',
         ];
+    }
+
+    /**
+     * Get the assets for the user.
+     */
+    public function assets(): HasMany
+    {
+        return $this->hasMany(Asset::class);
     }
 }
