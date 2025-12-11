4d351b0968cad1bdde28058a9ee39ecd017987f4
Sushant Aryal <sushant.aryal90@gmail.com>
2025-12-11 22:50:34 +0545

    Update user factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Asset;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +18,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()
+            ->has(Asset::factory())
+            ->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ]);
+        
+        User::factory()
+            ->has(Asset::factory())
+            ->create([
+                'name' => 'Test User 2',
+                'email' => 'test2@example.com',
+            ]);
     }
 }
