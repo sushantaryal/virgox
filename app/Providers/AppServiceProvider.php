69ec0e9213c35c5e53758b7a41c64a973a820dd3
Sushant Aryal <sushant.aryal90@gmail.com>
2025-12-12 20:40:54 +0545

    Bind interface to repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Contracts\AssetRepositoryInterface;
+use App\Contracts\OrderRepositoryInterface;
+use App\Contracts\UserRepositoryInterface;
+use App\Repositories\AssetRepository;
+use App\Repositories\OrderRepository;
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(AssetRepositoryInterface::class, AssetRepository::class);
     }
 
     /**
